602381415152cd640ad91574b56cedd71be88bef
SmartDevOnBlock <115705859+SmartDevOnBlock@users.noreply.github.com>
2023-03-31 12:16:25 +0300

    Update about.jsx

--- pages/about.jsx
+++ pages/about.jsx
@@ -2,8 +2,8 @@ const AboutPage = () => {
   return (
     <>
       <h2>A Little Bit About Me</h2>
-      <h3><br/>I am a <b>junior web3 developer<b/> with an active mind, high ambitions and determination. <br/> I am passionate about blockchain and decentralized applications.
-    <br/><br/> I can create smart contracts using <mark>"Solidity"<mark/> language, Back-end with <mark>"Node.js"<mark/> and connect them to the Front-end, which I develop with <mark>"React"<mark/> library and <mark>"Next.js"<mark/>. I can use both standard <strong>CSS modules<strong/>, <strong>SCSS<strong/> or <strong>Tailwind<strong/> and <strong>Styled-Components<strong/> frameworks to work with design.
+      <h3><br/>I am a <b>junior web3 developer</b> with an active mind, high ambitions and determination. <br/> I am passionate about blockchain and decentralized applications.
+    <br/><br/> I can create smart contracts using <mark>"Solidity"</mark> language, Back-end with <mark>"Node.js"</mark> and connect them to the Front-end, which I develop with <mark>"React"</mark> library and <mark>"Next.js"</mark>. I can use both standard <strong>CSS modules</strong>, <strong>SCSS</strong> or <strong>Tailwind</strong> and <strong>Styled-Components</strong> frameworks to work with design.
 
 
 <br/><br/>As a person, I am <i>responsible</i>, <i>punctual</i>, <i>communicative</i> and <i>well-educated</i>. I adore a creative approach to problem solving. I always dive deep into the tasks at hand, think outside the box and don't limit myself to existing solutions, trying to provide my own. </h3>
