74117f0d698cb966e8300416af76cef4cf48489c
SmartDevOnBlock <115705859+SmartDevOnBlock@users.noreply.github.com>
2023-03-31 12:14:41 +0300

    Update about.jsx

--- pages/about.jsx
+++ pages/about.jsx
@@ -6,7 +6,7 @@ const AboutPage = () => {
     <br/><br/> I can create smart contracts using <mark>"Solidity"<mark/> language, Back-end with <mark>"Node.js"<mark/> and connect them to the Front-end, which I develop with <mark>"React"<mark/> library and <mark>"Next.js"<mark/>. I can use both standard <strong>CSS modules<strong/>, <strong>SCSS<strong/> or <strong>Tailwind<strong/> and <strong>Styled-Components<strong/> frameworks to work with design.
 
 
-<br/><br/>As a person, I am <i>responsible<i/>, <i>punctual<i/>, <i>communicative<i/> and <i>well-educated<i/>. I adore a creative approach to problem solving. I always dive deep into the tasks at hand, think outside the box and don't limit myself to existing solutions, trying to provide my own. </h3>
+<br/><br/>As a person, I am <i>responsible</i>, <i>punctual</i>, <i>communicative</i> and <i>well-educated</i>. I adore a creative approach to problem solving. I always dive deep into the tasks at hand, think outside the box and don't limit myself to existing solutions, trying to provide my own. </h3>
     </>
   );
 };
