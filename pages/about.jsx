49cd14097b25d6aa1bd2ab1799368965e6b506a6
SmartDevOnBlock <115705859+SmartDevOnBlock@users.noreply.github.com>
2023-03-31 12:12:46 +0300

    Update about.jsx

--- pages/about.jsx
+++ pages/about.jsx
@@ -2,11 +2,11 @@ const AboutPage = () => {
   return (
     <>
       <h2>A Little Bit About Me</h2>
-      <h3>I am a junior web3 developer with an active mind, high ambitions and determination. <br/> I am passionate about blockchain and decentralized applications.
-    <br/> I can create smart contracts using "Solidity" language, Back-end with "Node.js" and connect them to the Front-end, which I develop with "React" library and "Next.js". I can use both standard CSS modules, SCSS or Tailwind and Styled-Components frameworks to work with design.
+      <h3><br/>I am a <b>junior web3 developer<b/> with an active mind, high ambitions and determination. <br/> I am passionate about blockchain and decentralized applications.
+    <br/><br/> I can create smart contracts using <mark>"Solidity"<mark/> language, Back-end with <mark>"Node.js"<mark/> and connect them to the Front-end, which I develop with <mark>"React"<mark/> library and <mark>"Next.js"<mark/>. I can use both standard <strong>CSS modules<strong/>, <strong>SCSS<strong/> or <strong>Tailwind<strong/> and <strong>Styled-Components<strong/> frameworks to work with design.
 
 
-<br/><br/>As a person, I am responsible, punctual, communicative and well-educated. I adore a creative approach to problem solving. I always dive deep into the tasks at hand, think outside the box and don't limit myself to existing solutions, trying to provide my own. </h3>
+<br/><br/>As a person, I am <i>responsible<i/>, <i>punctual<i/>, <i>communicative<i/> and <i>well-educated<i/>. I adore a creative approach to problem solving. I always dive deep into the tasks at hand, think outside the box and don't limit myself to existing solutions, trying to provide my own. </h3>
     </>
   );
 };
